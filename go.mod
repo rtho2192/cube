9435a7be365403a92af0fd27eec99e90f8de8fa4
SynchroTiger <>
2021-07-12 23:14:01 +0100

    Skeleton Created

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rtho2192/cube
+
+go 1.16
+
+require (
+	github.com/docker/go-connections v0.4.0
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3 // indirect
+	github.com/google/uuid v1.2.0
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rtho2192/cube/node"
+	"github.com/rtho2192/cube/task"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+
+	"github.com/rtho2192/cube/manager"
+	"github.com/rtho2192/cube/worker"
+)
+
+func main() {
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+
+	w := worker.Worker{
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]task.Task),
+	}
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]task.Task),
+		EventDb: make(map[string][]task.TaskEvent),
+	}
+
+	fmt.Printf("manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("nodeL %v\n", n)
+}
